389b8cc055c74dd4c774a23b695f312ada1f9804
mjLib1217 <mzecatjrotus1217@gmail.com>
2020-12-20 23:48:57 +0900

    Add Triangle

--- Web Standard/Canvas/Sample2/main.js
+++ Web Standard/Canvas/Sample2/main.js
@@ -6,3 +6,15 @@ document.body.appendChild(canvas);
 
 const context = canvas.getContext('2d');
 
+context.beginPath();
+context.moveTo(10, 10);
+context.lineTo(10, 35);
+context.lineTo(35, 10);
+context.fill();
+
+context.beginPath();
+context.moveTo(40, 40);
+context.lineTo(40, 15);
+context.lineTo(15, 40);
+context.closePath();
+context.stroke();
