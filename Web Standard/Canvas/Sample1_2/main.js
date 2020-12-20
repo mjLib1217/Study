4e7aeb73d509ea901501e79efb159f9b33e769d6
mjLib1217 <mzecatjrotus1217@gmail.com>
2020-12-20 23:51:36 +0900

    Add Circle

--- Web Standard/Canvas/Sample1_2/main.js
+++ Web Standard/Canvas/Sample1_2/main.js
@@ -5,3 +5,15 @@ canvas.width = 500;
 document.body.appendChild(canvas);
 
 const context = canvas.getContext('2d');
+
+context.beginPath();
+context.moveTo(140, 90);
+context.arc(90, 90, 50, 0, Math.PI * 2, true);
+context.moveTo(125, 90);
+context.arc(90, 90, 35, 0, Math.PI, false);
+context.moveTo(80, 75);
+context.arc(75, 75, 5, 0, Math.PI * 2, true);
+context.moveTo(110, 75);
+context.arc(105, 75, 5, 0, Math.PI * 2, true);
+context.closePath();
+context.stroke();
